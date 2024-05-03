3ed8c7e803eec0979b692bb5fe3a24ef3be72f7e
shishX86 <kykyky@bk.ru>
2024-05-03 09:01:28 +0300

    drag and drop

--- resources/js/controllers/editor_controller.js
+++ resources/js/controllers/editor_controller.js
@@ -1,7 +1,8 @@
 import EditorJS from '@editorjs/editorjs';
-import Header from '@editorjs/header'; 
+import Header from '@editorjs/header';
 import RawTool from '@editorjs/raw';
 import SimpleImage from "@editorjs/simple-image";
+import DragDrop from 'editorjs-drag-drop';
 
 export default class extends window.Controller {
     connect() {
@@ -13,14 +14,19 @@ export default class extends window.Controller {
              */
             holder: id,
 
-            tools: { 
+            tools: {
                 header: {
-                  class: Header, 
-                  inlineToolbar: ['link'] 
+                    class: Header,
+                    inlineToolbar: ['link']
                 },
                 raw: RawTool,
                 image: SimpleImage,
-            }, 
+
+            },
+
+            onReady: () => {
+                new DragDrop(editor);
+            },
 
             onChange: (api, event) => {
                 editor.save().then((outputData) => {
@@ -30,16 +36,5 @@ export default class extends window.Controller {
                 });
             }
         });
-
-        // const btn = document.getElementById('editorjs-btn')
-        
-        // btn.addEventListener('click', () => {
-        //     editor.save().then((outputData) => {
-        //         document.getElementById('editorjs-input').value = JSON.stringify(outputData)
-        //         console.log('Article data: ', outputData)
-        //     }).catch((error) => {
-        //         console.log('Saving failed: ', error)
-        //     });
-        // })
     }
 }
